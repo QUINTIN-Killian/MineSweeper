2822ab40e0c7071bc0de6990f0f6fe5673853aa3
Killian QUINTIN <killian.quintin@epitech.eu>
2024-08-20 02:48:52 +0200

    [ADD] -h

--- src/main.c
+++ src/main.c
@@ -29,10 +29,29 @@ void debug(void)
     mini_printf("\e[33mDEBUG\n\e[0m");
 }
 
-int main(void)
+static int help(void)
+{
+    mini_printf("Welcome in MineSweeper !\n\n");
+    mini_printf("Your goal is to reveal the content of all the rocks ");
+    mini_printf("underground except the ones containing bombs which must be ");
+    mini_printf("flaged.\n");
+    mini_printf("The number found in a rock indicates the number of ");
+    mini_printf("adjacent bombs.\n");
+    mini_printf("\t- Press MOUSE LEFT to dig a rock\n");
+    mini_printf("\t- Press MOUSE RIGHT to flag a rock\n");
+    mini_printf("\t- Press H to show help\n");
+    mini_printf("\t- Press SPACE BAR to turn off/on the musics\n");
+    mini_printf("\t- Press UP/DOWN to manage the musics volume\n");
+    mini_printf("\n(Game created by Killian QUINTIN)\n");
+    return 0;
+}
+
+int main(int ac, char **av)
 {
     game_t game;
 
+    if (ac == 2 && my_strcmp(av[1], "-h") == 0)
+        return help();
     srand(time(NULL));
     init(&game);
     print_map(&game);
